dcb8f3a631a64650f4ee026d9721a4236ea3d0e3
T.R. Brooks <trbrooks@gmail.com>
2018-05-16 14:51:23 -0400

    Update Exp1_plots_analyses.R

--- Exp1_plots_analyses.R
+++ Exp1_plots_analyses.R
@@ -13,6 +13,7 @@ setwd("/yourfilepath/")
 df1 <- readRDS("weight.rds")
 
 # the weight.rda dataframe (tibble) has 6 variables in "long" format
+# all IVs are within subjects
 # -----IVs-----
 # Participant: participant number
 # Arc:         wielding mode (Full = forehand 90 degrees, Half = forehand-backhand 45 degrees)
@@ -48,11 +49,15 @@ df4 <- bind_cols(means,sds[3])
 SEs <- as.tibble(within_error)
 df5 <- inner_join(SEs[,c(1:2,4:7)],df4, by = c("Mass", "Spin"))
 
+# Plot means and standard error for all 9 conditions of unidirectional wielding mode
+
 p1 <- ggplot(subset(df3,Arc=="Full"), aes(x=Mass,y=Judgment, group=Spin, color=Spin)) +
   geom_line() +
   geom_point() +
   theme(legend.position="none") +
   geom_errorbar(aes(ymin=Judgment-se, ymax=Judgment+se, color=Spin), width=.05)
+
+# Plot means and standard error for all 9 conditions of bidirectional wielding mode
 
 p2 <- ggplot(subset(df3,Arc=="Half"), aes(x=Mass,y=Judgment, group=Spin, color=Spin)) +
   geom_line() +
@@ -60,14 +65,20 @@ p2 <- ggplot(subset(df3,Arc=="Half"), aes(x=Mass,y=Judgment, group=Spin, color=S
   theme(legend.position="none") +
   geom_errorbar(aes(ymin=Judgment-se, ymax=Judgment+se, color=Spin), width=.05)
 
+# Plot means and standard error for all 9 conditions of both wielding modes
+
 p3 <- ggplot(SEs, aes(x=Mass,y=Judgment, group=Spin, color=Spin)) +
   geom_line() +
   geom_point() +
   theme(legend.justification=c(2,-.5), legend.position=c(1,0)) +
   geom_errorbar(aes(ymin=Judgment-se, ymax=Judgment+se, color=Spin), width=.05)
 
+# Arrange plots
+
 row2 <- plot_grid(p1, p2, labels = c("B", "C"), nrow=1)
 plot_grid(p3, row2, ncol=1, labels=c('A',''))
+
+# Mixed effects ANOVA with Spin, Mass, and Spin x Arc fixed effects and Participant as random effect.
 
 mod1 <- aov(Judgment ~ Spin + Mass + Spin:Arc + Error(Participant), data=df1)
 
